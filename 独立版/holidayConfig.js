99eb0af588856a970736542a2190ce0ec8ab7931
fangyihang <290341190@qq.com>
2019-01-14 09:36:53 +0800

    Update holidayConfig.js

--- 独立版/holidayConfig.js
+++ 独立版/holidayConfig.js
@@ -6,13 +6,13 @@
 var holiday = {
     y2019: [[date = 20190101, mark = '元旦'], [date = 20190102, mark = '元旦'], [date = 20190204, mark = '春节'], [date = 20190205, mark = '春节'], [date = 20190206, mark = '春节'], [date = 20190207, mark = '春节'], [date = 20190208, mark = '春节'], [date = 20190209, mark = '春节'], [date = 20190210, mark = '春节'], [date = 20190405, mark = '清明节'], [date = 20190406, mark = '清明节'], [date = 20190407, mark = '清明节'], [date = 20190501, mark = '劳动节'], [date = 20190607, mark = '端午节'], [date = 20190608, mark = '端午节'], [date = 20190609, mark = '端午节'], [date = 20190913, mark = '中秋节'], [date = 20190914, mark = '中秋节'], [date = 20190915, mark = '中秋节'], [date = 20191001, mark = '国庆节'], [date = 20191002, mark = '国庆节'], [date = 20191003, mark = '国庆节'], [date = 20191004, mark = '国庆节'], [date = 20191005, mark = '国庆节'], [date = 20191006, mark = '国庆节'], [date = 20191007, mark = '国庆节']],
     y2018: [[date = 20180101, mark = '元旦'], [date = 20180215, mark = '春节'], [date = 20180216, mark = '春节'], [date = 20180217, mark = '春节'], [date = 20180218, mark = '春节'], [date = 20180219, mark = '春节'], [date = 20180220, mark = '春节'], [date = 20180221, mark = '春节'], [date = 20180405, mark = '清明节'], [date = 20180406, mark = '清明节'], [date = 20180407, mark = '清明节'], [date = 20180429, mark = '劳动节'], [date = 20180430, mark = '劳动节'], [date = 20180501, mark = '劳动节'], [date = 20180618, mark = '端午节'], [date = 20180924, mark = '中秋节'],  [date = 20191001, mark = '国庆节'], [date = 20191002, mark = '国庆节'], [date = 20191003, mark = '国庆节'], [date = 20191004, mark = '国庆节'], [date = 20191005, mark = '国庆节'], [date = 20191006, mark = '国庆节'], [date = 20191007, mark = '国庆节']],
-}
+};
 
 //双休日上班的
 var workday = {
     y2019: [[date = 20190202, mark = '春节'], [date = 20190203, mark = '春节'], [date = 20190929, mark = '国庆'], [date = 20191012, mark = '国庆']],
     y2018: [[date = 20180211, mark = '春节'], [date = 20180224, mark = '春节'], [date = 20180408, mark = '清明节'], [date = 20180428, mark = '劳动节'], [date = 20180929, mark = '国庆'], [date = 20180930, mark = '国庆']],
-}
+};
 
 //-----------------------------------------------------------------------
 // Part of the LINQ to JavaScript (JSLINQ) v2.10 Project - http://jslinq.codeplex.com
@@ -202,14 +202,13 @@ var workday = {
     JSLINQ.fn.init.prototype = JSLINQ.fn;
 })();
 
-
+//-----------------------------------------------------------------------------------正式代码段--------------------------------------------------------------------//
 
 /**
  * @method 判断当前时间的节假日类型
  * @returns {int} 0:工作日，1：双休日（不包括法定不休的） ，2：法定节假日
  * @desc 根据目标对象获取运营商
  */
-
 Date.prototype.isHoliday = function () {
     var date = this;
     if (JSLINQ(holiday['y' + date.getFullYear()]).Where(function (item) { return item.date == parseInt(date.Format("yyyyMMdd")); })
@@ -229,7 +228,6 @@ Date.prototype.isHoliday = function () {
  * @returns {Date} 最终时间
  * @desc 根据目标对象获取运营商
  */
-
 Date.prototype.getWorkday = function (num) {
     var date = this;
     if (num > 0) {
@@ -272,5 +270,36 @@ Date.prototype.Format = function (fmt) { //author: meizz
         if (new RegExp("(" + k + ")").test(fmt))
             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
-}  
+}
+
+/**
+ * @method
+ * @param {int} num 加减时间 
+ * @desc 延用C的习惯
+ */
+Date.prototype.addSeconds = function (num) {
+    if(typeof(num)!="number") throw "num is not number!"
+    this.setSeconds(this.getSeconds()+num);
+}
+
+Date.prototype.addMinutes = function (num) {
+    if(typeof(num)!="number") throw "num is not number!"
+    this.setMinutes(this.getMinutes()+num);
+}
+
+Date.prototype.addHours = function (num) {
+    if(typeof(num)!="number") throw "num is not number!"
+    this.setHours(this.getHours()+num);
+}
+
+Date.prototype.addDays = function (num) {
+    if(typeof(num)!="number") throw "num is not number!"
+    this.setDate(this.getDate()+num);
+}
+
+Date.prototype.addMonths = function (num) {
+    if(typeof(num)!="number") throw "num is not number!"
+    this.setMonth(this.getMonth()+num);
+}
+
 
